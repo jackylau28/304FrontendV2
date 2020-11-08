50eabd3d9d4df8eada40a23274476433acd59208
Netsoft <jacky@netsoft.net>
2020-11-08 19:26:07 +0800

    Create home page carousel

--- src/main.js
+++ src/main.js
@@ -24,6 +24,10 @@ Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 import Card from './components/Card.vue'
 Vue.component('m-card', Card)
 
+import axios from 'axios'
+Vue.prototype.$http=axios.create({
+    baseURL:'http://localhost:3000/web/api'
+})
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <swiper ref="mySwiper" :options="swiperOptions">
+    <!--swiper-- ref="mySwiper" :options="swiperOptions">
       <swiper-slide>
         <img class="w-100" src="../assets/images/s1.jpg" alt="" />
       </swiper-slide>
@@ -11,16 +11,28 @@
         <img class="w-100" src="../assets/images/s1.jpg" alt="" />
       </swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
-    </swiper>
+    </!--swiper-->
 
-    <el-carousel :interval="5000" type="card" height="200px">
-      <el-carousel-item v-for="banner in banners" :key="banner">
-        <!--img class="w-100" src="../assets/images/s1.jpg" alt=""-->
-        <img :src="banner" />
+     <!--el-carousel trigger="click" :height="bannerHeight + 'px'">
+      <el-carousel-item v-for="item in banners" :key="item.id">
+        <img
+          :src="baseurl + item.backdrop_path"
+          alt="banner"
+          ref="bannerHeight"
+          width="300px"
+          @load="imgLoad"
+        >
       </el-carousel-item>
-    </el-carousel>
+    </el-carousel-->
 
-    
+   <el-carousel :interval="4000" type="card"  >
+    <el-carousel-item v-for="item in banners" :key="item.id">
+        <a href="item.url" :target="item.isNewPage ? '_self' : '_blank'">
+            <h3 class="medium d-flex centered">{{ item.original_title }}</h3>
+            <img alt="banner"  :src="baseurl + item.backdrop_path" width="100%" >
+        </a>
+    </el-carousel-item>
+  </el-carousel>   
 
     <!-- news -->
     <m-card icon="menu" title="News">
@@ -37,7 +49,9 @@
 export default {
   data() {
     return {
-      banners: ["../assets/images/s1.jpg", "../assets/images/s2.jpg"],
+        baseurl:'https://image.tmdb.org/t/p/original',
+        bannerHeight: '',
+      banners: '',
       swiperOptions: {
         pagination: {
           el: ".swiper-pagination",
@@ -45,6 +59,36 @@ export default {
         // Some Swiper option/callback...
       },
     };
+  },
+  methods:{
+      async fetchNewsCat (){
+          const res_new= await this.$http.get('news/list')
+          console.log(res_new)
+      },
+      async fetchMovie(){
+          const response= await this.$http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=05dc3d92066f96e0187d56c1c2be6673&language=zh-tw')
+          //return {banners: response.data.data}
+          console.log(response.data.results)   
+          this.banners=  response.data.results
+        //return {banners: response.data.results}
+      },
+  
+      imgLoad() {
+        this.$nextTick(() => {
+          this.bannerHeight = this.$refs.bannerHeight[0].height
+        })
+      }
+  },
+    mounted() {
+        this.imgLoad()
+        window.addEventListener('resize', () => {
+        this.bannerHeight = this.$refs.bannerHeight[0].height
+        this.imgLoad()
+        }, false)
+    },
+  created() {
+      //this.fetchNewsCat()
+      this.fetchMovie()
   },
 };
 </script>
@@ -56,5 +100,12 @@ export default {
 
 .el-carousel__item:nth-child(2n + 1) {
   background-color: #d3dce6;
+}
+.centered {
+  position: absolute;
+  top: 90%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
 }
 </style>
